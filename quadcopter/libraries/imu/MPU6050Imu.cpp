26fb30e79ff04cee1d2f33a94e3ec72c8a75ec86
Chris Burgess <chris.burgess2010@gmail.com>
2014-11-26 02:41:25 -0800

    Update to using more efficient burst reads from the IMU registers.

--- quadcopter/libraries/imu/MPU6050Imu.cpp
+++ quadcopter/libraries/imu/MPU6050Imu.cpp
@@ -32,19 +32,25 @@ uint16_t Mpu6050Imu::read_mpu_6050_register(uint8_t reg) {
 void Mpu6050Imu::read_raw(int16_t *accel_x, int16_t *accel_y, int16_t *accel_z,
              int16_t *gyro_x,  int16_t *gyro_y,  int16_t *gyro_z) {
 
+    // accelx, accely, accelz, temp, gyrox, gyroy, gyroz
+    uint16_t rawdata[7];
 
-    // Read the raw data from the 9-axis
-    *gyro_x = read_mpu_6050_register(GYRO_XOUT_H) << 8;
-    *gyro_x |= read_mpu_6050_register(GYRO_XOUT_L);
-    *gyro_y = read_mpu_6050_register(GYRO_YOUT_H) << 8;
-    *gyro_y |= read_mpu_6050_register(GYRO_YOUT_L);
-    *gyro_z = read_mpu_6050_register(GYRO_ZOUT_H) << 8;
-    *gyro_z |= read_mpu_6050_register(GYRO_ZOUT_L);
-    *accel_x = read_mpu_6050_register(ACCEL_XOUT_H) << 8;
-    *accel_x |= read_mpu_6050_register(ACCEL_XOUT_L);
-    *accel_y = read_mpu_6050_register(ACCEL_YOUT_H) << 8;
-    *accel_y |= read_mpu_6050_register(ACCEL_YOUT_L);
-    *accel_z = read_mpu_6050_register(ACCEL_ZOUT_H) << 8;
-    *accel_z |= read_mpu_6050_register(ACCEL_ZOUT_L);
+    // Perform a burst read of all the registers. A burst read of registers will
+    // grab the data from the specified register initially. The register address
+    // will be incremented for each subsequent byte.  Thus all data can be 
+    // acquired in 2 I2C transactions as opposed to 24!
+    const char start_register = ACCEL_XOUT_H;
+    _i2c.write((MPU_6050_SLAVE_ADDR<<1), (const char*)&start_register, 1);
+    _i2c.read((MPU_6050_SLAVE_ADDR<<1), (char*)rawdata, 14);
+
+
+    // Copy the values in. Sadly, the register values are essentially given to 
+    // us in big-endian format whereas the Cortex M4 is little-endian.
+    *accel_x = ((rawdata[0] & 0x00FF) << 8) | ((rawdata[0] & 0xFF00) >> 8);
+    *accel_y = ((rawdata[1] & 0x00FF) << 8) | ((rawdata[1] & 0xFF00) >> 8);
+    *accel_z = ((rawdata[2] & 0x00FF) << 8) | ((rawdata[2] & 0xFF00) >> 8);
+    *gyro_x = ((rawdata[4] & 0x00FF) << 8) | ((rawdata[4] & 0xFF00) >> 8);
+    *gyro_y = ((rawdata[5] & 0x00FF) << 8) | ((rawdata[5] & 0xFF00) >> 8);
+    *gyro_z = ((rawdata[6] & 0x00FF) << 8) | ((rawdata[6] & 0xFF00) >> 8);
     
 }
