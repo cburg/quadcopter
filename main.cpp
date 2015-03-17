48ac45e020f7094665a40cfe7b80e0efb4647945
Chris Burgess <chrisb@cetoncorp.com>
2015-03-17 00:39:04 -0700

    Add simple control of a motor and reporting of IMU values.

--- main.cpp
+++ main.cpp
@@ -1,33 +1,85 @@
 
 #include "mbed.h"
 #include "quadcopter/libraries/servo/PWMServo.h"
+#include "quadcopter/libraries/imu/MPU6050Imu.h"
 
+
+RawSerial pc(USBTX, USBRX);
+Mpu6050Imu imu(I2C_SDA, I2C_SCL);
+PWMServo motor(PWM_OUT);
+uint32_t current_pwm_us;
+
+#define PWM_MIN     900
+#define PWM_MAX     1900
+#define PWM_IDLE    
+
+// Motor Initialization:
+//  1.) Set stick to idle
+//  2.) Connect Battery
+//  3.) Hold in Idle for 1.0 seconds
+//  4.) Wait for beeps
+//  5.) Increase motor speed to desired level.
+
+void serial_interrupt() {
+    char c = pc.getc();
+    switch(c) {
+        case(' '): // Kill Switch!
+            motor.set_pwm_value(0);
+            printf("Killing Motor!!!\n\r");
+            break;
+        case('m'):    // Min
+            current_pwm_us = PWM_MIN;
+            motor.set_pwm_value(current_pwm_us);
+            printf("Setting motor speed to: %d\n\r", (int)current_pwm_us);
+            break;
+        case('/'):    // Max
+            current_pwm_us = PWM_MAX;
+            motor.set_pwm_value(current_pwm_us);
+            printf("Setting motor speed to: %d\n\r", (int)current_pwm_us);
+            break;
+        case('.'):    // Step Up
+            current_pwm_us += 5;
+            if (current_pwm_us > PWM_MAX) {
+                current_pwm_us = PWM_MAX;
+            }
+            motor.set_pwm_value(current_pwm_us);
+            printf("Setting motor speed to: %d\n\r", (int)current_pwm_us);
+            break;
+        case(','):    // Step Down
+            current_pwm_us -= 5;
+            if (current_pwm_us < PWM_MIN) {
+                current_pwm_us = PWM_MIN;
+            }
+            motor.set_pwm_value(current_pwm_us);
+            printf("Setting motor speed to: %d\n\r", (int)current_pwm_us);
+            break;
+        default:
+            break;
+    }
+}
 
 int main() {
-    RawSerial pc(USBTX, USBRX);
+    pc.attach(serial_interrupt);
     pc.baud(115200);
 
-    InterruptIn sonar_in();
+    imu.set_sensitivity(Sensitivity_1, Sensitivity_1);
 
-    PWMServo ms(PC_9);
-    ms.set_min_max_pwm(575, 2425);
-    ms.set_min_max_angle(0, 180);
+    motor.set_min_max_pwm(1100, 1900);
+    motor.set_period_ms(20);
+
+    current_pwm_us = 0;
+
+    int16_t accel_x = 0;
+    int16_t accel_y = 0;
+    int16_t accel_z = 0;
+    int16_t gyro_x = 0;
+    int16_t gyro_y = 0;
+    int16_t gyro_z = 0;
 
     while(true) {
-        int i;
-        for (i = 0; i <= 60; i += 5) {
-            ms.move_to_angle(i);
-            pc.printf("%d\n\r", i);
-            wait_ms(100);
-        }
-
-            ms.move_to_angle(60);
-            ms.move_to_angle(120);
-
-        for (i = 120; i >= 0; i -= 5) {
-            ms.move_to_angle(i);
-            pc.printf("%d\n\r", i);
-            wait_ms(100);
-        }
+        imu.read_raw(&accel_x, &accel_y, &accel_z, &gyro_x, &gyro_y, &gyro_z);
+        pc.printf("Ax: %d\tAy: %d\tAz: %d\tGx: %d\tGy: %d\tGz: %d\t\n\r",
+            accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z);
+        wait_ms(20);
     }
 }
